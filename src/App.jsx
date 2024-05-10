40551a24bfb39231551a672ddfac46dc44833b3d
solangeihirwe03 <solangeduhimbazeihirwe@gmail.com>
2024-05-10 10:23:02 +0200

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from "./components.jsx/Homepage";
-import Login from "./components.jsx/Login";
-import Signup from "./components.jsx/Signup";
-import Farmer from "./components.jsx/Farmer";
-import Profile from "./components.jsx/Profile";
-import Layout from "./components.jsx/Layout";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Signup" element={<Signup />} />
-        <Route element={<Layout />}>
-          <Route path="/Farmer" element={<Farmer />} />
-          <Route path="/Profile" element={<Profile />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
-=======
 import Header from "./components/Header"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -34,26 +9,38 @@ import Search from "./pages/Search";
 import AboutUs from "./pages/AboutUs";
 import Contact from "./pages/Contact";
 import LearnMore from "./pages/LearnMore";
+import Homepage from "./pages/Homepage";
+import Login from './pages/Homepage';
+import Signup from './pages/Signup';
+import Layout from './pages/LayoutFarm';
+import Farmer from './pages/Farmer';
+import Profile from './pages/Profile'
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Home/>}/>
-          <Route path="/about-us" element={<AboutUs/>}/>
-          <Route path="/shop" element={<Product/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/search" element={<Search/>}/>
-          <Route path="/cart" element={<Cart/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path="/learnmore" element={<LearnMore/>}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route element={<Layoutfarm />}>
+          <Route path="/Farmer" element={<Farmer />} />
+          <Route path="/Profile" element={<Profile />} />
+        </Route>
+        <Route path="/shop" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/shop" element={<Product />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/learnmore" element={<LearnMore />} />
         </Route>
       </Routes>
     </Router>
-  )
->>>>>>> 853d03bae0ab30ce55446b92c46f14ba18fadba6
+  );
 }
 
 export default App;
