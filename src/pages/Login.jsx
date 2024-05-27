c17a3d373212722bcc8a0ecf84671f7c9c574b96
Sugiramarynnn <sugimarylene@gmail.com>
2024-05-27 13:21:37 +0300

    Changed signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,11 +12,6 @@ const Login = () => {
         <h1 className="text-2xl font-bold sm:text-3xl animate-bounce duration-200">
           Get started today!
         </h1>
-
-        {/* <p className="mt-4 text-gray-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero
-          nulla eaque error neque ipsa culpa autem, at itaque nostrum!
-        </p> */}
       </div>
 
       <form action="#" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
@@ -106,7 +101,7 @@ const Login = () => {
             type="Login"
             className="inline-block rounded-lg bg-[#FF9C00] px-5 py-3 text-sm font-medium text-white"
           >
-            <a href="/Profile">Log in</a>
+            <a href="">Log in</a>
           </button>
         </div>
       </form>
