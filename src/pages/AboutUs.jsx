bb7a4ede6bfd221476b4d47f78d18d7d821c143c
Sugiramarynnn <sugimarylene@gmail.com>
2024-05-28 12:48:59 +0300

    dfghjk

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import about from "../../public/test.jpg";
-import Footer from "./Footer";
 import ab1 from "../../public/1 about.jpg";
 import ab2 from "../../public/2 about.jpg";
 import { useSpring, animated } from "react-spring";
@@ -17,6 +16,7 @@ const AboutUs = () => {
     });
     return <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>;
   };
+
   return (
     <div>
       <div>
@@ -33,39 +33,42 @@ const AboutUs = () => {
             zIndex: "-1",
             position: "absolute",
           }}
-          className=" border border-blue-950 pb-56"
+          className="border border-blue-950 pb-56"
         ></div>
         <div className="mt-20">
-          <h1 className="flex justify-center  text-white font-extrabold text-lg">
+          <h1 className="flex justify-center text-white font-extrabold text-lg">
             About Us
           </h1>
-          <h3 className="flex justify-center"></h3>
         </div>
       </div>
 
-      <div className="mt-72 border  bg-[#F3F6F0]  p-24">
-        <div className="flex  space-x-16">
-          <div className="border bg-white border-transparent shadow-lg p-24">
-            <h1 className="text-[#45AB49] font-bold">Our visssion</h1>
-            <p className="mt-8 font-semibold">
-              To become the leading global platform that connects consumers with
-              the finest organic produce, fostering a sustainable future by
-              promoting the benefits of organic farming and supporting
-              specialized farmers worldwide. We envision a world where healthy,
-              organic food is accessible to all, where consumers are empowered
-              to make informed choices about their nutrition, and where farmers
-            </p>
+      <div className="mt-20 md:mt-32 lg:mt-72 border bg-[#F3F6F0] p-4 md:p-12 lg:p-24">
+        <div className="flex flex-col md:flex-row md:space-x-16">
+          <div className="mb-8 md:mb-0 md:w-1/2">
+            <div className="border bg-white border-transparent shadow-lg p-4 md:p-12">
+              <h1 className="text-[#45AB49] font-bold">Our Vision</h1>
+              <p className="mt-4 md:mt-8 font-semibold">
+                To become the leading global platform that connects consumers with
+                the finest organic produce, fostering a sustainable future by
+                promoting the benefits of organic farming and supporting
+                specialized farmers worldwide. We envision a world where healthy,
+                organic food is accessible to all, where consumers are empowered
+                to make informed choices about their nutrition, and where farmers
+              </p>
+            </div>
           </div>
-          <div className="border border-transparent shadow-lg bg-white p-24">
-            <h1 className="text-[#45AB49] font-bold">Our Mission</h1>
-            <p className="mt-8 font-semibold">
-              Our mission is to unearth and deliver top-tier organic products,
-              including fruits, vegetables, and more, from specialized farmers
-              to our customers. We are committed to ensuring the highest quality
-              and sustainability standards while empowering farmers and
-              enriching the health and well-being of our consumers through
-              fresh, organic produce.
-            </p>
+          <div className="md:w-1/2">
+            <div className="border bg-white border-transparent shadow-lg p-4 md:p-12">
+              <h1 className="text-[#45AB49] font-bold">Our Mission</h1>
+              <p className="mt-4 md:mt-8 font-semibold">
+                Our mission is to unearth and deliver top-tier organic products,
+                including fruits, vegetables, and more, from specialized farmers
+                to our customers. We are committed to ensuring the highest quality
+                and sustainability standards while empowering farmers and
+                enriching the health and well-being of our consumers through
+                fresh, organic produce.
+              </p>
+            </div>
           </div>
         </div>
         <div
@@ -79,30 +82,30 @@ const AboutUs = () => {
             height: "80%",
             width: "100%",
           }}
-          className="  flex justify-around text-2xl font-bold text-white p-24 my-[8rem]"
+          className="flex justify-around text-2xl font-bold text-white p-4 md:p-12 my-8 md:my-[8rem]"
         >
           <div>
             <Number n={1500} />
-            <h4 className="text-white"> Satisfied Clients</h4>
+            <h4 className="text-white">Satisfied Clients</h4>
           </div>
           <div>
             <Number n={2000} />
-            <h4 className="text-white">Our products</h4>
+            <h4 className="text-white">Our Products</h4>
           </div>
           <div>
             <Number n={1200} />
-            <h4 className="text-white">Awards Wining</h4>
+            <h4 className="text-white">Award Winning</h4>
           </div>
         </div>
-        <div className="my-[12rem] flex gap-[8rem]">
-          <div className=" w-[40rem]">
-            <p className="text-orange-400 font-bold mb-5 text-2xl">
+        <div className="my-8 md:my-[12rem] flex flex-col md:flex-row md:gap-16">
+          <div className="md:w-1/2">
+            <p className="text-orange-400 font-bold mb-2 md:mb-5 text-2xl">
               WHY CHOOSE US{" "}
             </p>
-            <p className="text-2xl font-bold mt-2"> Organic & Healthy</p>
-            <p className="text-2xl font-bold mt-2"> Foods Provider Farm</p>
+            <p className="text-2xl font-bold mt-2">Organic & Healthy</p>
+            <p className="text-2xl font-bold mt-2">Foods Provider Farm</p>
 
-            <p className="mt-8 font-bold">
+            <p className="mt-4 md:mt-8 font-semibold">
               There many variations of lorem ipsum available, but the majority
               have suffered <br />
               alterration in some form, by injected humour, or randomised words
@@ -112,7 +115,7 @@ const AboutUs = () => {
             <br />
 
             <br />
-            <div className="bg-white  border rounded-lg">
+            <div className="bg-white border rounded-lg p-4 md:p-8">
               <p>Vegetables & Fruits</p>
               <p>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
@@ -122,17 +125,19 @@ const AboutUs = () => {
               </p>
             </div>
           </div>
-          <div className="transfrom duration-45 -rotate-12 ml-[8rem]">
-            <img
-              src={ab1}
-              alt=""
-              className="w-[20rem]  rounded-full border-8 border-[#FF9C00]"
-            />
-            <img
-              src={ab2}
-              alt=""
-              className="w-[20rem] h-[20rem] -mt-[8rem] ml-5 rounded-full border-8 border-white"
-            />
+          <div className="md:w-1/2 flex justify-center items-center">
+            <div className="transform duration-500 hover:rotate-12">
+              <img
+                src={ab1}
+                alt=""
+                className="w-40 md:w-[20rem] rounded-full border-8 border-[#FF9C00] mb-4 md:mb-0"
+              />
+              <img
+                src={ab2}
+                alt=""
+                className="w-40 md:w-[20rem] h-40 md:h-[20rem] rounded-full border-8 border-white"
+              />
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,54 +1,132 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
 import { IoPerson } from "react-icons/io5";
 import { FaCartShopping } from "react-icons/fa6";
+import { CiMenuFries } from "react-icons/ci";
+
+const Menu = ({ isOpen, toggleMenu }) => {
+  return (
+    <ul
+      id="mobileNav"
+      className={`absolute flex flex-col space-y-2 bg-white text-black top-16 left-0 w-full hover:text-[#45AB49] ${
+        isOpen ? "block" : "hidden"
+      }`}
+    >
+      <li>
+        <a href="/" className="flex items-center py-2 px-4">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="/AboutUs" className=" flex items-center py-2 px-4 ">
+          About Us
+        </a>
+      </li>
+
+      <li>
+        <a href="/Shop" className="flex items-center py-2 px-4">
+          Shop
+        </a>
+      </li>
+      <li>
+        <a href="/Contact" className="flex items-center py-2 px-4">
+          Contact Us
+        </a>
+      </li>
+
+      <li>
+        <a href="/Product" className="flex items-center py-2 px-4">
+          {<IoPerson className="w-[20px]" />}
+        </a>
+      </li>
+      <li>
+        <a href="/Cart" className="flex items-center py-2 px-4">
+          {<FaCartShopping className="w-[20px]" />}
+        </a>
+      </li>
+
+      <li>
+        <button
+          className="md:flex  py-2 px-2 bg-[#45AB49] text-white font-medium rounded"
+          onClick={toggleMenu}
+        >
+          <a href="/Login" className="flex items-center px-4 py-2">
+            Login
+          </a>
+        </button>
+      </li>
+    </ul>
+  );
+};
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <div>
-      <div className="font-bold flex justify-between px-[10%] py-4 items-center text-lg bg-white  z-20">
-        <Link>
-          <p className="text-4xl ">
+      <div className=" fixed bg-white drop-shadow-lg text-black font-bold top-0 left-0 w-full text-grey-500  flex justify-around items-center z-40">
+        <div className="flex items-center">
+          <p className="text-4xl font-semibold ">
             <span className="text-[#45AB49]">AGRI</span>sales
           </p>
-        </Link>
-        <ul className="flex items-center gap-3">
-          <li className="flex space-x-4">
-            <div className="flex justify-center items-center space-x-8">
-              <NavLink to="/" className={"hover:text-[#45AB49]"}>
-                Home
-              </NavLink>
-              <NavLink to="/AboutUs" className={"hover:text-[#45AB49]"}>
-                About Us
-              </NavLink>
+        </div>
 
-              <NavLink to="/Shop" className={"hover:text-[#45AB49]"}>
-                Shop
-              </NavLink>
-              <NavLink to="/Contact" className="hover:text-[#45AB49]">
-                Contact Us
-              </NavLink>
+        <ul className="hidden md:flex space-x-4  hover:text-{#45ab49}">
+          <li>
+            <a href="/" className="flex items-center py-2 px-4">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/AboutUs" className=" flex items-center py-2 px-4 ">
+              About Us
+            </a>
+          </li>
 
-              <NavLink to="/Product" className={"hover:text-[#45AB49]"}>
-                {<IoPerson className="w-[20px]" />}
-              </NavLink>
+          <li>
+            <a href="/Shop" className="flex items-center py-2 px-4">
+              Shop
+            </a>
+          </li>
+          <li>
+            <a href="/Contact" className="flex items-center py-2 px-4">
+              Contact Us
+            </a>
+          </li>
 
-              <NavLink to="/cart" className={"hover:text-[#45AB49]"}>
-                <FaCartShopping className="w-[20px]" />
-              </NavLink>
+          <li>
+            <a href="/Product" className="flex items-center py-2 px-4">
+              {<IoPerson className="w-[20px]" />}
+            </a>
+          </li>
+          <li>
+            <a href="/Cart" className="flex items-center py-2 px-4">
+              {<FaCartShopping className="w-[20px]" />}
+            </a>
+          </li>
 
-              <li>
-                <NavLink
-                  to="/Login"
-                  className="md:flex  py-2 px-2 bg-[#45AB49] text-white font-medium rounded"
-                >
-                  Login
-                </NavLink>
-              </li>
-            </div>
+          <li>
+            <button
+              className="md:flex  py-2 px-2 bg-[#45AB49] text-black font-medium rounded"
+              onClick={toggleMenu}
+            >
+              <a href="/Login" className="flex items-center px-4 py-2">
+                Login
+              </a>
+            </button>
           </li>
         </ul>
+        <div className="md:hidden">
+          <button
+            id="menuToggle"
+            className="text-black font-bold focus:outline-none text-2xl"
+            onClick={toggleMenu}
+          >
+            <CiMenuFries />
+          </button>
+        </div>
+        <Menu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
       </div>
     </div>
   );
